fb7708cc6ae7994aaff5c206321f1d54e36fd162
Justin Fyfe <justin@fyfesoftware.ca>
2019-03-04 18:30:49 -0500

    Updated version codes

--- SanteDB.Caching.Memory/Properties/AssemblyInfo.cs
+++ SanteDB.Caching.Memory/Properties/AssemblyInfo.cs
@@ -50,10 +50,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.10.0.*")][assembly: AssemblyInformationalVersion("Gananoque")]
-[assembly: AssemblyVersion("1.10.0.*")]
+// [assembly: AssemblyVersion("1.20.0.*")][assembly: AssemblyInformationalVersion("Gananoque")]
+[assembly: AssemblyVersion("1.20.0.*")]
 [assembly: AssemblyInformationalVersion("Gananoque")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.20.0.0")]
 
 [assembly: Plugin(Group = "Performance", Environment = PluginEnvironment.Server, EnableByDefault = true)]
 [assembly: PluginDependency("SanteDB.Core, Version=1.10.0.0")]
--- SanteDB.Persistence.MDM.Test/Properties/AssemblyInfo.cs
+++ SanteDB.Persistence.MDM.Test/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("16be3000-c76a-41fc-99db-c5c90dd521e8")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.20.0.0")]
+[assembly: AssemblyFileVersion("1.20.0.0")]
