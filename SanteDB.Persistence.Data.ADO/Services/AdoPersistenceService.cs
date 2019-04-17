13191d2705cbfa0ccd54dc2c5494f0cbba79fbd2
Justin Fyfe <justin@fyfesoftware.ca>
2019-04-17 14:18:09 -0400

    Implementation of the ADO bis provider

--- SanteDB.Persistence.Data.ADO/Services/AdoPersistenceService.cs
+++ SanteDB.Persistence.Data.ADO/Services/AdoPersistenceService.cs
@@ -20,7 +20,9 @@
 using SanteDB.BI;
 using SanteDB.BI.Model;
 using SanteDB.BI.Services;
+using SanteDB.BI.Util;
 using SanteDB.Core;
+using SanteDB.Core.Configuration.Data;
 using SanteDB.Core.Diagnostics;
 using SanteDB.Core.Exceptions;
 using SanteDB.Core.Interfaces;
@@ -30,8 +32,10 @@ using SanteDB.Core.Model.Interfaces;
 using SanteDB.Core.Model.Map;
 using SanteDB.Core.Security;
 using SanteDB.Core.Security.Attribute;
+using SanteDB.Core.Security.Services;
 using SanteDB.Core.Services;
 using SanteDB.OrmLite;
+using SanteDB.OrmLite.Providers;
 using SanteDB.Persistence.Data.ADO.Configuration;
 using SanteDB.Persistence.Data.ADO.Data;
 using SanteDB.Persistence.Data.ADO.Data.Hax;
@@ -43,8 +47,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Diagnostics.Tracing;
+using System.Dynamic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 
 namespace SanteDB.Persistence.Data.ADO.Services
@@ -563,7 +569,106 @@ namespace SanteDB.Persistence.Data.ADO.Services
         /// </summary>
         public BisResultContext ExecuteQuery(BisQueryDefinition queryDefinition, Dictionary<string, object> parameters)
         {
-            throw new NotImplementedException();
+            if (queryDefinition == null)
+                throw new ArgumentNullException(nameof(queryDefinition));
+
+            // Add core context parameters if not already added
+            
+            // First we want to grab the connection strings used by this object
+            var filledQuery = BiUtils.ResolveRefs(queryDefinition);
+
+            // The ADO.NET provider only allows one connection to one db at a time, so verify the connections are appropriate
+            if (queryDefinition.DataSources?.Count != 1)
+                throw new InvalidOperationException($"ADO.NET BI queries can only source data from 1 connection source, query {queryDefinition.Name} has {queryDefinition.DataSources?.Count}");
+
+            // Ensure we have sufficient priviledge
+            var pdpService = ApplicationServiceContext.Current.GetService<IPolicyDecisionService>();
+            foreach (var pol in queryDefinition.DataSources.SelectMany(o => o.Demands).Union(queryDefinition.Demands))
+            {
+                var outcome = pdpService.GetPolicyOutcome(AuthenticationContext.Current.Principal, pol);
+                if (outcome != Core.Model.Security.PolicyGrantType.Grant)
+                    throw new PolicyViolationException(AuthenticationContext.Current.Principal, pol, outcome);
+            }
+
+            // Apply defaults where possible
+            foreach (var defaultParm in queryDefinition.Parameters.Where(p => !String.IsNullOrEmpty(p.DefaultValue) && !parameters.ContainsKey(p.Name)))
+                parameters.Add(defaultParm.Name, defaultParm.DefaultValue);
+
+            // Next we validate parameters
+            if (!queryDefinition.Parameters.Where(p => p.Required == true).All(p => parameters.ContainsKey(p.Name)))
+                throw new InvalidOperationException("Missing required parameter");
+
+            // Validate parameter values
+            foreach(var kv in parameters.ToArray())
+            {
+                var parmDef = queryDefinition.Parameters.FirstOrDefault(p => p.Name == kv.Key);
+                if (parmDef == null) continue; // skip
+                else switch(parmDef.Type)
+                    {
+                        case BisParameterDataType.Boolean:
+                            parameters[kv.Key] = Boolean.Parse(kv.Value.ToString());
+                            break;
+                        case BisParameterDataType.Date:
+                        case BisParameterDataType.DateTime:
+                            parameters[kv.Key] = DateTime.Parse(kv.Value.ToString());
+                            break;
+                        case BisParameterDataType.Integer:
+                            parameters[kv.Key] = Int32.Parse(kv.Value.ToString());
+                            break;
+                        case BisParameterDataType.String:
+                            parameters[kv.Key] = kv.Value.ToString();
+                            break;
+                        case BisParameterDataType.Uuid:
+                            parameters[kv.Key] = Guid.Parse(kv.Value.ToString());
+                            break;
+                        default:
+                            throw new InvalidOperationException($"Cannot determine how to parse {parmDef.Type}");
+                    }
+            }
+
+            // We want to open the specified connection
+            var connectionString = ApplicationServiceContext.Current.GetService<IConfigurationManager>().GetSection<DataConfigurationSection>().ConnectionString.FirstOrDefault(o => o.Name == queryDefinition.DataSources.First().ConnectionString);
+            var provider = Activator.CreateInstance(queryDefinition.DataSources.First().ProviderType) as IDbProvider;
+            provider.ConnectionString = connectionString.Value;
+            provider.ReadonlyConnectionString = connectionString.Value;
+
+            // Query definition
+            var rdbmsQueryDefinition = queryDefinition.QueryDefinitions.FirstOrDefault(o => o.Invariants.Contains(provider.Invariant));
+            if (queryDefinition == null)
+                throw new InvalidOperationException($"Could not find a query definition for invariant {provider.Invariant}");
+
+            // Prepare the templated SQL
+            var parmRegex = new Regex(@"\$\{(\w*?)\}");
+            List<Object> values = new List<object>();
+            var stmt = parmRegex.Replace(rdbmsQueryDefinition.Sql, (m) =>
+            {
+                object pValue = null;
+                parameters.TryGetValue(m.Groups[1].Value, out pValue);
+                values.Add(pValue);    
+                return "?";
+            });
+
+            // Get a readonly context
+            using (var context = provider.GetReadonlyConnection())
+            {
+                try
+                {
+                    context.Open();
+                    DateTime startTime = DateTime.Now;
+                    var results = context.Query<ExpandoObject>(new SqlStatement(provider, stmt, values));
+                    return new BisResultContext(
+                        queryDefinition,
+                        parameters,
+                        this,
+                        results,
+                        startTime);
+                }
+                catch(Exception e)
+                {
+                    this.m_tracer.TraceError("Error executing BIS data query: {0}", e);
+                    throw new DataPersistenceException($"Error executing BIS data query", e);
+                }
+            }
         }
 
         /// <summary>
