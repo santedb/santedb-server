7c71bcebb6a12ee1a1c33b91d68aa93b13e165a4
Justin Fyfe <justin@fyfesoftware.ca>
2019-06-24 08:25:48 -0400

    Rev'd versions to 1.52

--- SanteDB.Messaging.RISI/Properties/AssemblyInfo.cs
+++ SanteDB.Messaging.RISI/Properties/AssemblyInfo.cs
@@ -51,13 +51,13 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.31.0.*")][assembly: AssemblyInformationalVersion("Halifax")]
-[assembly: AssemblyVersion("1.31.0.*")]
+// [assembly: AssemblyVersion("1.52.0.*")][assembly: AssemblyInformationalVersion("Halifax")]
+[assembly: AssemblyVersion("1.52.0.*")]
 [assembly: AssemblyInformationalVersion("Halifax")]
-[assembly: AssemblyFileVersion("1.31.0.0")]
+[assembly: AssemblyFileVersion("1.52.0.0")]
 
 
 [assembly: Plugin(EnableByDefault = false, Environment = PluginEnvironment.Server, Group = FeatureGroup.Messaging)]
-[assembly: PluginDependency("SanteDB.Core, Version=1.31.0.0")]
-[assembly: PluginDependency("RestSrvr, Version=1.31.0.0")]
+[assembly: PluginDependency("SanteDB.Core, Version=1.52.0.0")]
+[assembly: PluginDependency("RestSrvr, Version=1.52.0.0")]
 [assembly: PluginTraceSource("SanteDB.Messaging.RISI")]
