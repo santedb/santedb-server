7f02e51bcebd72df437ebe3cf61c4fe8edf08415
Justin Fyfe <justin@fyfesoftware.ca>
2019-09-05 20:14:55 +0700

    Rev'd versions and fixed persistnece error on non versioned entity data

--- SanteDB.Caching.Redis/Properties/AssemblyInfo.cs
+++ SanteDB.Caching.Redis/Properties/AssemblyInfo.cs
@@ -53,8 +53,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.28.*")]
-[assembly: AssemblyVersion("1.67.0.*")]
-[assembly: AssemblyFileVersion("1.67.0.0")]
+[assembly: AssemblyVersion("1.68.0.*")]
+[assembly: AssemblyFileVersion("1.68.0.0")]
 
 [assembly:Plugin(Group = FeatureGroup.Performance, Environment = PluginEnvironment.Server, EnableByDefault = false)]
 [assembly:PluginDependency("SanteDB.Core, 1.10.0")]
