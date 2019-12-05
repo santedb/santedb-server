ce66f1d46fd32a38c0e2cb458410823b4edfc159
Justin Fyfe <justin@fyfesoftware.ca>
2019-12-05 17:12:44 -0500

    Rev'd nuget references

--- SanteDB.Messaging.HL7/Properties/AssemblyInfo.cs
+++ SanteDB.Messaging.HL7/Properties/AssemblyInfo.cs
@@ -53,10 +53,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.28.*")]
-[assembly: AssemblyVersion("1.79.0.0")]
-[assembly: AssemblyFileVersion("1.79.0.0")]
+[assembly: AssemblyVersion("1.81.0.0")]
+[assembly: AssemblyFileVersion("1.81.0.0")]
 
 
 [assembly: Plugin(EnableByDefault = false, Environment = PluginEnvironment.Server, Group = FeatureGroup.Messaging)]
-[assembly: PluginDependency("SanteDB.Core, Version=1.79.0.0")]
+[assembly: PluginDependency("SanteDB.Core, Version=1.81.0.0")]
 [assembly: PluginTraceSource(Hl7Constants.TraceSourceName)]
--- SanteDB.Persistence.Diagnostics.Jira/Properties/AssemblyInfo.cs
+++ SanteDB.Persistence.Diagnostics.Jira/Properties/AssemblyInfo.cs
@@ -51,11 +51,11 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.79.0.*")][assembly: AssemblyInformationalVersion("Halifax")]
-[assembly: AssemblyVersion("1.79.0.*")]
+// [assembly: AssemblyVersion("1.81.0.*")][assembly: AssemblyInformationalVersion("Halifax")]
+[assembly: AssemblyVersion("1.81.0.*")]
 [assembly: AssemblyInformationalVersion("Halifax")]
-[assembly: AssemblyFileVersion("1.79.0.0")]
+[assembly: AssemblyFileVersion("1.81.0.0")]
 
 [assembly: Plugin(Group = FeatureGroup.Diagnostics, Environment = PluginEnvironment.Server, EnableByDefault = false)]
-[assembly: PluginDependency("SanteDB.Core, 1.79.0.0")]
+[assembly: PluginDependency("SanteDB.Core, 1.81.0.0")]
 [assembly: PluginTraceSource("SanteDB.Persistence.Diagnostics.Jira")]
