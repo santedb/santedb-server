acaec4b96ef39c11f55d0569a68b721d6bbab8cd
Justin Fyfe <justin@fyfesoftware.ca>
2020-06-07 15:40:49 -0400

    Updated to add logic to batch files to pull/push to/from branchs

--- submodule-pull.bat
+++ submodule-pull.bat
@@ -1,12 +1,17 @@
 @ECHO OFF
+IF [%1] == [] (
+	echo Must specify branch
+	goto end
+)
 	ECHO WILL PULL SUBMODULES
 	SET cwd = %cd%
 	FOR /D %%G IN (.\*) DO (
 		PUSHD %%G
 		IF EXIST ".git" (
-			ECHO Pulling %%G
-			git checkout master
+			ECHO Pulling %1 on %%G
+			git checkout %1
 			git pull
 		)
 		POPD
 	)
+:end
--- submodule-push.bat
+++ submodule-push.bat
@@ -1,17 +1,17 @@
 @ECHO OFF
 
-IF %1=="" (
-	ECHO MISSING COMMIT MESSAGE
+IF %2=="" (
+	ECHO USE SUBMODULE-PUSH BRANCH MESSAGE
 ) ELSE (
-	ECHO WILL UPDATE SUBMODULES WITH "%1"
+	ECHO WILL UPDATE SUBMODULES ON "%1"
 	SET cwd = %cd%
 	FOR /D %%G IN (.\*) DO (
 		PUSHD "%%G"
 		IF EXIST ".git" (
 			ECHO Pushing %%G
-			git checkout master
+			git checkout %1
 			git add *
-			git commit -am %1
+			git commit -am %2
 			git pull
 			git push
 			git push --tags
