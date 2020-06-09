254866eaff73cd1ea304d23111162252a314c8c7
Justin Fyfe <justin@fyfesoftware.ca>
2020-06-09 10:59:09 -0400

    Updated version codes

--- SanteDB.Caching.Memory.Test/Properties/AssemblyInfo.cs
+++ SanteDB.Caching.Memory.Test/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.18.*")][assembly: AssemblyInformationalVersion("2.0.18")]
-[assembly: AssemblyVersion("2.0.18.*")]
-[assembly: AssemblyInformationalVersion("2.0.18")]
-[assembly: AssemblyFileVersion("2.0.18.0")]
+// [assembly: AssemblyVersion("2.0.19.*")][assembly: AssemblyInformationalVersion("2.0.19")]
+[assembly: AssemblyVersion("2.0.19.*")]
+[assembly: AssemblyInformationalVersion("2.0.19")]
+[assembly: AssemblyFileVersion("2.0.19.0")]
--- SanteDB.Caching.Memory/Properties/AssemblyInfo.cs
+++ SanteDB.Caching.Memory/Properties/AssemblyInfo.cs
@@ -51,10 +51,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.18.*")][assembly: AssemblyInformationalVersion("2.0.18")]
-[assembly: AssemblyVersion("2.0.18.*")]
-[assembly: AssemblyInformationalVersion("2.0.18")]
-[assembly: AssemblyFileVersion("2.0.18.0")]
+// [assembly: AssemblyVersion("2.0.19.*")][assembly: AssemblyInformationalVersion("2.0.19")]
+[assembly: AssemblyVersion("2.0.19.*")]
+[assembly: AssemblyInformationalVersion("2.0.19")]
+[assembly: AssemblyFileVersion("2.0.19.0")]
 
 [assembly: Plugin(Group = FeatureGroup.Performance, Environment = PluginEnvironment.Server, EnableByDefault = true)]
 [assembly: PluginDependency("SanteDB.Core, Version=1.10.0.0")]
