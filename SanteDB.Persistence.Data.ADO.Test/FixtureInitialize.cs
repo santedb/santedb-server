fdbb99df0801a4e45d7f04bbb7ab855deb8f82e2
Justin Fyfe <justin@fyfesoftware.ca>
2021-03-01 19:19:24 -0500

    Refactored to NUNIT

---
+++ SanteDB.Persistence.Data.ADO.Test/FixtureInitialize.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SanteDB.Core.Security;
+using SanteDB.Core.TestFramework;
+using SanteDB.Persistence.Data.ADO.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanteDB.Persistence.Data.ADO.Tests
+{
+    [SetUpFixture]
+    public class FixtureInitialize
+    {
+        [OneTimeSetUp]
+        public void ClassSetup()
+        {
+            TestApplicationContext.TestAssembly = typeof(AdoIdentityProviderTest).Assembly;
+            TestApplicationContext.Initialize(TestContext.CurrentContext.TestDirectory);
+
+            AuthenticationContext.Current = new AuthenticationContext(AuthenticationContext.SystemPrincipal);
+        }
+    }
+}
