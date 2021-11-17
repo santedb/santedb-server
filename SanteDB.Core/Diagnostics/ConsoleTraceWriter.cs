62468838c6dcc2ac379e178320b1a267db3c1ac4
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-16 20:07:57 -0500

    Updated version codes - refactored the common trace writers out

--- SanteDB.Core/Diagnostics/ConsoleTraceWriter.cs
+++ SanteDB.Core/Diagnostics/ConsoleTraceWriter.cs
@@ -36,104 +36,14 @@ namespace SanteDB.Server.Core.Diagnostics
     /// Tracer writer that writes to the console
     /// </summary>
     [DisplayName("Console Trace Writer")]
-    public class ConsoleTraceWriter : TraceWriter, IDisposable
+    [Obsolete("Use SanteDB.Core.Diagnostics.Tracing.ConsoleTraceWriter", true)]
+    public class ConsoleTraceWriter : SanteDB.Core.Diagnostics.Tracing.ConsoleTraceWriter
     {
-        // Dispatch thread
-        private Thread m_dispatchThread = null;
-
-        // True when disposing
-        private bool m_disposing = false;
-
-        // The log backlog
-        private ConcurrentQueue<KeyValuePair<ConsoleColor, String>> m_logBacklog = new ConcurrentQueue<KeyValuePair<ConsoleColor, string>>();
-
-        // Reset event
-        private ManualResetEventSlim m_resetEvent = new ManualResetEventSlim(false);
-
         /// <summary>
-        /// Console trace writer
+        /// Passthrough constructor
         /// </summary>
-        public ConsoleTraceWriter(EventLevel filter, string initializationData, IDictionary<String, EventLevel> sources) : base(filter, initializationData, sources)
+        public ConsoleTraceWriter(EventLevel filter, string initializationData, IDictionary<string, EventLevel> sources) : base(filter, initializationData, sources)
         {
-            // Start log dispatch
-            this.m_dispatchThread = new Thread(this.LogDispatcherLoop);
-            this.m_dispatchThread.IsBackground = true;
-            this.m_dispatchThread.Start();
-        }
-
-        /// <summary>
-        /// Write a trace
-        /// </summary>
-        protected override void WriteTrace(EventLevel level, string source, string format, params object[] args)
-        {
-            ConsoleColor color = ConsoleColor.White;
-            switch (level)
-            {
-                case EventLevel.Verbose:
-                    if (format.Contains("PERF"))
-                        color = ConsoleColor.Green;
-                    else
-                        color = ConsoleColor.Magenta;
-                    break;
-
-                case EventLevel.Informational:
-                    color = ConsoleColor.Cyan;
-                    break;
-
-                case EventLevel.Warning:
-                    color = ConsoleColor.Yellow;
-                    break;
-
-                case EventLevel.Error:
-                    color = ConsoleColor.Red;
-                    break;
-
-                case EventLevel.Critical:
-                    color = ConsoleColor.White;
-                    Console.BackgroundColor = ConsoleColor.DarkRed;
-                    break;
-            }
-
-            this.m_logBacklog.Enqueue(new KeyValuePair<ConsoleColor, String>(color, String.Format("{0:yyyy/MM/dd HH:mm:ss} [{1}] : {2} {3}: 0 : {4}", DateTime.Now, String.IsNullOrEmpty(Thread.CurrentThread.Name) ? $"@{Thread.CurrentThread.ManagedThreadId}" : Thread.CurrentThread.Name, source, level, String.Format(format, args))));
-            this.m_resetEvent.Set();
-        }
-
-        private void LogDispatcherLoop()
-        {
-            while (true)
-            {
-                while (this.m_logBacklog.IsEmpty && !this.m_disposing)
-                {
-                    this.m_resetEvent.Wait();
-                    this.m_resetEvent.Reset();
-                }
-                if (this.m_disposing) return;
-
-                while (!this.m_logBacklog.IsEmpty)
-                {
-                    if (this.m_logBacklog.TryDequeue(out var dq))
-                    {
-                        if (this.m_disposing) return;
-                        Console.ForegroundColor = dq.Key;
-                        Console.WriteLine(dq.Value);
-                        Console.ResetColor();
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Dispose of the object
-        /// </summary>
-        public void Dispose()
-        {
-            if (this.m_dispatchThread != null)
-            {
-                this.m_disposing = true;
-                this.m_resetEvent.Set();
-                this.m_dispatchThread = null;
-            }
-            Console.ResetColor();
         }
     }
 }
--- SanteDB.Core/Diagnostics/SystemDiagnosticsTraceWriter.cs
+++ SanteDB.Core/Diagnostics/SystemDiagnosticsTraceWriter.cs
@@ -18,6 +18,7 @@
  * User: fyfej
  * Date: 2021-8-27
  */
+
 using SanteDB.Core.Diagnostics;
 using System;
 using System.Collections.Generic;
@@ -34,54 +35,8 @@ namespace SanteDB.Server.Core.Diagnostics
     /// Represents a trace writer to Tracer
     /// </summary>
     [DisplayName("System/Debugger Trace Writer")]
-    public class SystemDiagnosticsTraceWriter : TraceWriter
+    [Obsolete("Use SanteDB.Core.Diagnostics.Tracing.SystemDiagnosticsTraceWriter", true)]
+    public class SystemDiagnosticsTraceWriter : SanteDB.Core.Diagnostics.Tracing.SystemDiagnosticsTraceWriter
     {
-
-        // Trace source
-        private TraceSource m_traceSource = new TraceSource("SanteDB");
-
-        /// <summary>
-        /// CTOR for diagnostics
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <param name="fileName"></param>
-        public SystemDiagnosticsTraceWriter(EventLevel filter, string fileName, IDictionary<String, EventLevel> sources) : base(filter, fileName, sources)
-        {
-        }
-
-        /// <summary>
-        /// Creates a new diagnostics trace writer
-        /// </summary>
-        public SystemDiagnosticsTraceWriter() : base (EventLevel.LogAlways, null, new Dictionary<String, EventLevel>())
-        {
-        }
-        
-        /// <summary>
-        /// Write the specified trace
-        /// </summary>
-        protected override void WriteTrace(EventLevel level, string source, string format, params object[] args)
-        {
-            TraceEventType eventLvl = TraceEventType.Information;
-            switch (level)
-            {
-                case EventLevel.Critical:
-                    eventLvl = TraceEventType.Critical;
-                    break;
-                case EventLevel.Error:
-                    eventLvl = TraceEventType.Error;
-                    break;
-                case EventLevel.Informational:
-                    eventLvl = TraceEventType.Information;
-                    break;
-                case EventLevel.Verbose:
-                    eventLvl = TraceEventType.Verbose;
-                    break;
-                case EventLevel.Warning:
-                    eventLvl = TraceEventType.Warning;
-                    break;
-            }
-
-            this.m_traceSource.TraceEvent(eventLvl, 0, format, args);
-        }
     }
 }
