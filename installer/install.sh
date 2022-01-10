345d7172909cb0869f7ff42b68036aabd448d4c0
Justin Fyfe <justin@fyfesoftware.ca>
2022-01-10 13:01:02 -0500

    Changed the cert install to CA

--- installer/install.sh
+++ installer/install.sh
@@ -106,7 +106,7 @@ $SUDO cp -rf * $INSTALL_ROOT
 
 echo "Installing Certificates "
 $SUDO mono $INSTALL_ROOT/SanteDB.exe --install-certs 
-$SUDO certmgr -add -c -m Trust inter.cer
+$SUDO certmgr -add -c -m CA inter.cer
 
 read_yesno "Do you want me to install SanteDB as a daemon?" daemon
 
