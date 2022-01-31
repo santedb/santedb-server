fed7d568739e0f7d73053d35995e6ab28e94f532
Justin Fyfe <justin@fyfesoftware.ca>
2022-01-31 12:46:39 -0500

    Revert change of release name -

--- SanteDB.Messaging.HDSI.Test/Properties/AssemblyInfo.cs
+++ SanteDB.Messaging.HDSI.Test/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.1.3.*")][assembly: AssemblyInformationalVersion("Regina")]
-[assembly: AssemblyInformationalVersion("Regina")]
+// [assembly: AssemblyVersion("2.1.3.*")][assembly: AssemblyInformationalVersion("Queenston")]
+[assembly: AssemblyInformationalVersion("Queenston")]
--- SanteDB.Persistence.Diagnostics.Jira/Properties/AssemblyInfo.cs
+++ SanteDB.Persistence.Diagnostics.Jira/Properties/AssemblyInfo.cs
@@ -53,7 +53,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.1.3.*")][assembly: AssemblyInformationalVersion("Regina")]
-[assembly: AssemblyInformationalVersion("Regina")]
+// [assembly: AssemblyVersion("2.1.3.*")][assembly: AssemblyInformationalVersion("Queenston")]
+[assembly: AssemblyInformationalVersion("Queenston")]
 [assembly: Plugin(Group = FeatureGroup.Diagnostics, Environment = PluginEnvironment.Server, EnableByDefault = false)]
 [assembly: PluginTraceSource("SanteDB.Persistence.Diagnostics.Jira")]
