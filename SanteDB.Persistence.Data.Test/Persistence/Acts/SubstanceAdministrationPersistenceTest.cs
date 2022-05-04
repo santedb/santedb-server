e6bc08c47e4cba3455e5161e505bac50daf49d60
Justin Fyfe <justin@fyfesoftware.ca>
2022-05-04 10:10:01 -0400

    SCOM-78 #close Implemented Substance Administration

---
+++ SanteDB.Persistence.Data.Test/Persistence/Acts/SubstanceAdministrationPersistenceTest.cs
@@ -0,0 +1,122 @@
+﻿using SanteDB.Core.Model;
+using NUnit.Framework;
+using SanteDB.Core.Model.Acts;
+using SanteDB.Core.Model.Constants;
+using SanteDB.Core.Security;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SanteDB.Core.Exceptions;
+
+namespace SanteDB.Persistence.Data.Test.Persistence.Acts
+{
+    /// <summary>
+    /// Tests for SBADM
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    public class SubstanceAdministrationPersistenceTest : DataPersistenceTest
+    {
+
+        /// <summary>
+        /// Ensures that substance administrations with the proper persistence layer will store and load data
+        /// </summary>
+        [Test]
+        public void TestPersistWithProper()
+        {
+            using(AuthenticationContext.EnterSystemContext())
+            {
+                var sbadm = new SubstanceAdministration()
+                {
+                    ActTime = DateTimeOffset.Now.Date,
+                    DoseQuantity = (decimal)1,
+                    DoseUnitKey = UnitOfMeasureKeys.Dose,
+                    IsNegated = true,
+                    RouteKey = RouteOfAdministrationKeys.InjectionIntramuscular,
+                    SiteKey = AdministrationSiteKeys.LeftArm,
+                    MoodConceptKey = MoodConceptKeys.Intent
+                };
+
+                var afterInsert = base.TestInsert(sbadm);
+                Assert.AreEqual(RouteOfAdministrationKeys.InjectionIntramuscular, afterInsert.RouteKey);
+                Assert.AreEqual(AdministrationSiteKeys.LeftArm, afterInsert.SiteKey);
+                Assert.AreEqual(UnitOfMeasureKeys.Dose, afterInsert.DoseUnitKey);
+                Assert.IsNull(afterInsert.DoseUnit);
+                Assert.IsNotNull(afterInsert.LoadProperty(o => o.DoseUnit));
+                Assert.IsNotNull(afterInsert.LoadProperty(o => o.Route ));
+                Assert.IsNotNull(afterInsert.LoadProperty(o => o.Site));
+                Assert.IsTrue(afterInsert.IsNegated);
+
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == true && o.DoseQuantity == 1 && o.DoseUnitKey == UnitOfMeasureKeys.Dose, 1);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 1 && o.DoseUnitKey == UnitOfMeasureKeys.Dose, 0);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == true && o.DoseQuantity == 2 && o.DoseUnitKey == UnitOfMeasureKeys.Dose, 0);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == true && o.DoseQuantity == 1 && o.DoseUnitKey == UnitOfMeasureKeys.Each, 0);
+                var afterQuery = base.TestQuery<SubstanceAdministration>(o => o.IsNegated == true && o.DoseQuantity == 1 && o.DoseUnitKey == UnitOfMeasureKeys.Dose, 1).First();
+                Assert.AreEqual(RouteOfAdministrationKeys.InjectionIntramuscular, afterQuery.RouteKey);
+                Assert.AreEqual(AdministrationSiteKeys.LeftArm, afterQuery.SiteKey);
+                Assert.AreEqual(UnitOfMeasureKeys.Dose, afterQuery.DoseUnitKey);
+                Assert.AreEqual(1, afterQuery.DoseQuantity);
+                Assert.AreEqual(MoodConceptKeys.Intent, afterQuery.MoodConceptKey);
+                Assert.IsNull(afterQuery.DoseUnit);
+                Assert.IsNotNull(afterQuery.LoadProperty(o => o.DoseUnit));
+                Assert.IsNotNull(afterQuery.LoadProperty(o => o.Route));
+                Assert.IsNotNull(afterQuery.LoadProperty(o => o.Site));
+                Assert.IsTrue(afterQuery.IsNegated);
+
+                // Test update
+                var afterUpdate = base.TestUpdate(afterQuery, o =>
+                {
+                    o.IsNegated = false;
+                    o.MoodConceptKey = ActMoodKeys.Eventoccurrence;
+                    o.DoseQuantity = 100;
+                    o.DoseUnitKey = UnitOfMeasureKeys.Percentage;
+                    return o;
+                });
+                Assert.IsFalse(afterUpdate.IsNegated);
+                Assert.AreEqual(ActMoodKeys.Eventoccurrence, afterUpdate.MoodConceptKey);
+
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == true && o.DoseQuantity == 1 && o.DoseUnitKey == UnitOfMeasureKeys.Dose, 0);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 1 && o.DoseUnitKey == UnitOfMeasureKeys.Dose, 0);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == true && o.DoseQuantity == 2 && o.DoseUnitKey == UnitOfMeasureKeys.Dose, 0);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == true && o.DoseQuantity == 1 && o.DoseUnitKey == UnitOfMeasureKeys.Each, 0);
+                afterQuery = base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 100 && o.DoseUnitKey == UnitOfMeasureKeys.Percentage && o.MoodConceptKey == MoodConceptKeys.Eventoccurrence, 1).First();
+
+                // Test delete
+                base.TestDelete(afterQuery, Core.Services.DeleteMode.LogicalDelete);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 100 && o.DoseUnitKey == UnitOfMeasureKeys.Percentage && o.MoodConceptKey == MoodConceptKeys.Eventoccurrence, 0);
+                afterQuery = base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 100 && o.DoseUnitKey == UnitOfMeasureKeys.Percentage && o.MoodConceptKey == MoodConceptKeys.Eventoccurrence && o.ObsoletionTime != null, 1).First();
+
+                // Test un-delete
+                base.TestUpdate(afterQuery, o =>
+                {
+                    return o;
+                });
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 100 && o.DoseUnitKey == UnitOfMeasureKeys.Percentage && o.MoodConceptKey == MoodConceptKeys.Eventoccurrence, 1);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 100 && o.DoseUnitKey == UnitOfMeasureKeys.Percentage && o.MoodConceptKey == MoodConceptKeys.Eventoccurrence && o.ObsoletionTime != null, 0);
+
+                // Perma delete
+                base.TestDelete(afterUpdate, Core.Services.DeleteMode.PermanentDelete);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 100 && o.DoseUnitKey == UnitOfMeasureKeys.Percentage && o.MoodConceptKey == MoodConceptKeys.Eventoccurrence, 0);
+                base.TestQuery<SubstanceAdministration>(o => o.IsNegated == false && o.DoseQuantity == 100 && o.DoseUnitKey == UnitOfMeasureKeys.Percentage && o.MoodConceptKey == MoodConceptKeys.Eventoccurrence && o.ObsoletionTime != null, 0);
+
+                // Should not update
+                try
+                {
+                    base.TestUpdate(afterQuery, o => o);
+                    Assert.Fail("Should throw exception!");
+                }
+                catch(DataPersistenceException d) when (d.InnerException is KeyNotFoundException) { }
+                catch
+                {
+                    Assert.Fail("Incorrect exception type thrown");
+                }
+
+
+            }
+        }
+
+    }
+}
---
+++ SanteDB.Persistence.Data/Services/Persistence/Acts/SubstanceAdministrationPersistenceService.cs
@@ -0,0 +1,66 @@
+﻿using SanteDB.Core.Model;
+using SanteDB.Core.Model.Acts;
+using SanteDB.Core.Services;
+using SanteDB.OrmLite;
+using SanteDB.Persistence.Data.Model.Acts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanteDB.Persistence.Data.Services.Persistence.Acts
+{
+    /// <summary>
+    /// Class which can persist and manage substance administrations in the database 
+    /// </summary>
+    public class SubstanceAdministrationPersistenceService : ActDerivedPersistenceService<SubstanceAdministration, DbSubstanceAdministration>
+    {
+        /// <summary>
+        /// DI constructor
+        /// </summary>
+        public SubstanceAdministrationPersistenceService(IConfigurationManager configurationManager, ILocalizationService localizationService, IAdhocCacheService adhocCacheService = null, IDataCachingService dataCachingService = null, IQueryPersistenceService queryPersistence = null) : base(configurationManager, localizationService, adhocCacheService, dataCachingService, queryPersistence)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override SubstanceAdministration BeforePersisting(DataContext context, SubstanceAdministration data)
+        {
+            data.DoseUnitKey = this.EnsureExists(context, data.DoseUnit)?.Key ?? data.DoseUnitKey;
+            data.RouteKey = this.EnsureExists(context, data.Route)?.Key ?? data.RouteKey;
+            data.SiteKey = this.EnsureExists(context, data.Site)?.Key ?? data.SiteKey;
+            return base.BeforePersisting(context, data);
+        }
+
+        /// <inheritdoc/>
+        protected override SubstanceAdministration DoConvertToInformationModelEx(DataContext context, DbActVersion dbModel, params object[] referenceObjects)
+        {
+            var retVal = base.DoConvertToInformationModelEx(context, dbModel, referenceObjects);
+            var dbSubst = referenceObjects.OfType<DbSubstanceAdministration>().FirstOrDefault();
+            if(dbSubst == null)
+            {
+                this.m_tracer.TraceWarning("Using slow loading for substance administration (hint: use the correct persistence service instead)");
+                dbSubst = context.FirstOrDefault<DbSubstanceAdministration>(o => o.ParentKey == dbModel.VersionKey);
+            }
+
+            // Loading mode
+            switch(DataPersistenceControlContext.Current?.LoadMode ?? this.m_configuration.LoadStrategy)
+            {
+                case LoadMode.FullLoad:
+                    if (dbSubst != null)
+                    {
+                        retVal.DoseUnit = retVal.DoseUnit.GetRelatedPersistenceService().Get(context, dbSubst.DoseUnitConceptKey);
+                        retVal.SetLoaded(o => o.DoseQuantity);
+                        retVal.Route = retVal.Route.GetRelatedPersistenceService().Get(context, dbSubst.RouteConceptKey);
+                        retVal.SetLoaded(o => o.Route);
+                        retVal.Site = retVal.Site.GetRelatedPersistenceService().Get(context, dbSubst.SiteConceptKey);
+                    }
+                    break;
+            }
+
+            // Copy other properties to the return value
+            retVal.CopyObjectData(this.m_modelMapper.MapDomainInstance<DbSubstanceAdministration, SubstanceAdministration>(dbSubst), declaredOnly: true);
+            return retVal;
+
+        }
+    }
+}
