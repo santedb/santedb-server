63f5c177adbcbac5aae791ae51cd9b4e1a9f8f96
Justin Fyfe <justin@fyfesoftware.ca>
2022-09-10 17:08:30 -0400

    Added new mail message interfaces

---
+++ SanteDB.Persistence.Data/Services/Persistence/Mail/MailMessagePersistenceService.cs
@@ -0,0 +1,39 @@
+﻿using SanteDB.Core.i18n;
+using SanteDB.Core.Mail;
+using SanteDB.Core.Services;
+using SanteDB.OrmLite;
+using SanteDB.Persistence.Data.Model.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanteDB.Persistence.Data.Services.Persistence.Mail
+{
+    /// <summary>
+    /// Mail message persistence service which can handles the persistence of <see cref="MailMessage"/> with 
+    /// <see cref="DbMailMessage"/>
+    /// </summary>
+    public class MailMessagePersistenceService : BaseEntityDataPersistenceService<MailMessage, DbMailMessage>
+    {
+        /// <inheritdoc/>
+        public MailMessagePersistenceService(IConfigurationManager configurationManager, ILocalizationService localizationService, IAdhocCacheService adhocCacheService = null, IDataCachingService dataCachingService = null, IQueryPersistenceService queryPersistence = null) : base(configurationManager, localizationService, adhocCacheService, dataCachingService, queryPersistence)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override MailMessage DoConvertToInformationModel(DataContext context, DbMailMessage dbModel, params object[] referenceObjects)
+        {
+            var retVal = base.DoConvertToInformationModel(context, dbModel, referenceObjects);
+
+            switch(DataPersistenceControlContext.Current?.LoadMode ?? this.m_configuration.LoadStrategy)
+            {
+                case LoadMode.FullLoad:
+                    retVal.Mailboxes = retVal.Mailboxes.GetRelatedPersistenceService().Query(context, o => o.TargetKey == dbModel.Key).ToList();
+                    retVal.SetLoaded(o => o.Mailboxes);
+                    break;
+            }
+            return retVal;
+        }
+    }
+}
---
+++ SanteDB.Persistence.Data/Services/Persistence/Mail/MailboxPersistenceService.cs
@@ -0,0 +1,44 @@
+﻿using SanteDB.Core.Mail;
+using SanteDB.Core.Services;
+using SanteDB.OrmLite;
+using SanteDB.Persistence.Data.Model.Mail;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Persistence.Data.Services.Persistence.Mail
+{
+    /// <summary>
+    /// Persistence service that can persist and handle mailboxes
+    /// </summary>
+    public class MailboxPersistenceService : BaseEntityDataPersistenceService<Mailbox, DbMailbox>
+    {
+        /// <inheritdoc/>
+        public MailboxPersistenceService(IConfigurationManager configurationManager, ILocalizationService localizationService, IAdhocCacheService adhocCacheService = null, IDataCachingService dataCachingService = null, IQueryPersistenceService queryPersistence = null) : base(configurationManager, localizationService, adhocCacheService, dataCachingService, queryPersistence)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override Mailbox BeforePersisting(DataContext context, Mailbox data)
+        {
+            data.OwnerKey = this.EnsureExists(context, data.Owner)?.Key ?? data.OwnerKey;
+            return base.BeforePersisting(context, data);
+        }
+
+        /// <inheritdoc/>
+        protected override Mailbox DoConvertToInformationModel(DataContext context, DbMailbox dbModel, params object[] referenceObjects)
+        {
+            var retVal = base.DoConvertToInformationModel(context, dbModel, referenceObjects);
+
+            switch(DataPersistenceControlContext.Current?.LoadMode ?? this.m_configuration.LoadStrategy)
+            {
+                case LoadMode.FullLoad:
+                    retVal.Owner = retVal.Owner.GetRelatedPersistenceService().Get(context, dbModel.OwnerKey);
+                    retVal.SetLoaded(o => o.Owner);
+                    goto case LoadMode.SyncLoad;
+                case LoadMode.SyncLoad:
+                    retVal.Messages = retVal.Messages.GetRelatedPersistenceService().Query(context, o=>o.SourceEntityKey == dbModel.Key)
+            }
+        }
+    }
+}
