702332cb4d8f28a214a7b7fa1c5613c73eb85ef2
Jenkins Server <jenkins@fyfesoftware.ca>
2023-04-03 22:05:46 +0000

    Updated the signed release

--- sign-release.sh
+++ sign-release.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 sign_all_asms() {
-	for S in ./bin/$1/{SanteDB*.exe,SanteMPI*.exe,SanteIMS*.exe,SanteGuard*.exe,Sante*.exe,SanteDB*.dll,SanteMPI*.dll,SanteIMS*.dll,SanteGuard*.dll,Sante*.dll}; do
-        	if [ -f ${S} ]; then
+	if [ -d $1 ]; then
+	echo "Signing Assemblies in `pwd`/$1"
+	for S in $1/{SanteDB*.exe,SanteMPI*.exe,SanteIMS*.exe,SanteGuard*.exe,Sante*.exe,SanteDB*.dll,SanteMPI*.dll,SanteIMS*.dll,SanteGuard*.dll,Sante*.dll}; do
+        	if [ -f "${S}" ]; then
+			echo "Signing `pwd`/${S}"
 			id `whoami` | sed -E "s/(.{60}).*$/\1/" | signcode -spc ~/.secret/authenticode.spc -v ~/.secret/authenticode.pvk  -a sha1 -$ commercial -n SanteDB\ Server -i http://santesuite.com/ -t http://timestamp.digicert.com/scripts/timstamp.dll -tr 10 ${S}
                 fi
         done
+	fi
 }
 
 sign_all_recursive() {
-	if [ -d "Release/netstandard2.0" ]; then
-		sign_all_asms "Release/netstandard2.0"
-	elif [ -d "Release/net4.8" ]; then
-		sign_all_asms "Release/net4.8"
-	else
-		sign_all_asms "Release"
+	if [ -d "./bin/Release/netstandard2.0" ]; then
+		sign_all_asms "bin/Release/netstandard2.0"
+	elif [ -d "./bin/Release/net4.8" ]; then
+		sign_all_asms "bin/Release/net4.8"
+	elif [ -d "./bin/Release" ]; then
+		sign_all_asms "bin/Release"
 	fi
 	for D in *; do
 		if [ -d "${D}" ]; then
