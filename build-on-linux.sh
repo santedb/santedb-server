b4c0ef15947a1f6eccb864ccdd604a66566447dc
Jenkins Server <jenkins@fyfesoftware.ca>
2023-04-06 14:30:58 +0000

    Updated the build to accept a configuration parameter

--- build-on-linux.sh
+++ build-on-linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if (( $# < 2 ))
+if (( $# < 3 ))
 then
-	echo "Use: build-on-linux.sh VERSION_ID BRANCH_NAME"
+	echo "Use: build-on-linux.sh VERSION_ID BRANCH_NAME CONFIGURATION"
 	exit -1;
 fi;
 
@@ -22,56 +22,58 @@ git checkout $2
 git pull
 ./submodule-pull.sh $2
 msbuild /t:clean /t:restore santedb-server-ext.sln /p:VersionNumber=$1 || exit 911
-msbuild /t:build /p:Configuration=Release santedb-server-ext.sln /p:VersionNumber=$1 /p:NoFirebird=1 || exit 911
+msbuild /t:build /p:Configuration=$3 santedb-server-ext.sln /p:VersionNumber=$1 /p:NoFirebird=1 || exit 911
 
-./sign-release.sh
 
-# MSBUILD on linux doesn't copy over documentation files for dependent projects so we're going to copy them manually
-cp ./santedb-model/bin/Release/*.XML ./bin/Release/ 
-cp -v ./santedb-fhir/SanteDB.Messaging.FHIR/Data/* ./bin/Release/Data/
-cp -v ./santedb-hl7/SanteDB.Messaging.HL7/Data/* ./bin/Release/Data/
-cp -v ./santedb-gs1/SanteDB.Messaging.GS1/Data/* ./bin/Release/Data/
-cp -v ./santedb-mdm/SanteDB.Persistence.MDM/Data/* ./bin/Release/Data
-cp -v ./SanteDB/config/* ./bin/Release/config/
-mkdir -p ./bin/Release/data/SQL
-cp -rv ./SanteDB.Persistence.Data.ADO/Data/SQL/* ./bin/Release/Data/SQL/
+if [ $3 -eq 'Release' ]; then
+	./sign-release.sh
+	# MSBUILD on linux doesn't copy over documentation files for dependent projects so we're going to copy them manually
+	cp ./santedb-model/bin/Release/*.XML ./bin/Release/ 
+	cp -v ./santedb-fhir/SanteDB.Messaging.FHIR/Data/* ./bin/Release/Data/
+	cp -v ./santedb-hl7/SanteDB.Messaging.HL7/Data/* ./bin/Release/Data/
+	cp -v ./santedb-gs1/SanteDB.Messaging.GS1/Data/* ./bin/Release/Data/
+	cp -v ./santedb-mdm/SanteDB.Persistence.MDM/Data/* ./bin/Release/Data
+	cp -v ./SanteDB/config/* ./bin/Release/config/
+	mkdir -p ./bin/Release/data/SQL
+	cp -rv ./SanteDB.Persistence.Data.ADO/Data/SQL/* ./bin/Release/Data/SQL/
 
-mkdir santedb-server-$1
-cd santedb-server-$1
-cp ../installer/install.sh ./
-chmod 755 installer.sh
-cp ../bin/Release/*.dll ./
-cp ../bin/Release/*.config ./
-cp ../bin/Release/*.xml ./
-cp ../bin/Release/*.exe ./
-cp ../bin/Release/*.exe.config ./
-mkdir -p {Elbonia/Data,applets,Data/SQL,config,schema}
-cp ../bin/Release/Data/*.dataset ./Data
-cp ../bin/Release/Data/SQL/* ./Data/sql -r
-cp ../bin/Release/config/* ./config
-cp ../bin/Release/applets/*.pak ./applets
-cp ../SanteDB/Data/Demo/* Elbonia/Data
-cp ../SanteDB/data/*.FDB Elbonia
+	mkdir santedb-server-$1
+	cd santedb-server-$1
+	cp ../installer/install.sh ./
+	chmod 755 installer.sh
+	cp ../bin/Release/*.dll ./
+	cp ../bin/Release/*.config ./
+	cp ../bin/Release/*.xml ./
+	cp ../bin/Release/*.exe ./
+	cp ../bin/Release/*.exe.config ./
+	mkdir -p {Elbonia/Data,applets,Data/SQL,config,schema}
+	cp ../bin/Release/Data/*.dataset ./Data
+	cp ../bin/Release/Data/SQL/* ./Data/sql -r
+	cp ../bin/Release/config/* ./config
+	cp ../bin/Release/applets/*.pak ./applets
+	cp ../SanteDB/Data/Demo/* Elbonia/Data
+	cp ../SanteDB/data/*.FDB Elbonia
 
-cd ..
-rmdir ./bin/dist -r
-mkdir ./bin/dist -p
-tar cjvf ./bin/dist/santedb-server-$1.tar.bz2 santedb-server-$1
-tar czvf ./bin/dist/santedb-server-$1.tar.gz santedb-server-$1
-rm -r ./santedb-server-$1
+	cd ..
+	rmdir ./bin/dist -r
+	mkdir ./bin/dist -p
+	tar cjvf ./bin/dist/santedb-server-$1.tar.bz2 santedb-server-$1
+	tar czvf ./bin/dist/santedb-server-$1.tar.gz santedb-server-$1
+	rm -r ./santedb-server-$1
 
-# Test if ISCC exist and if wine is present (required for building the installers)
-if [ ! -f /usr/bin/wine ]; then
-	echo "Skipping packaging of Windows Installer (requires Wine)"
-	exit 0;
-fi;
-if [ ! -f /opt/inno/ISCC.exe ]; then
-	echo "Skipping packaging of Windows Installer (requires Inno Setup Compiler code at /opt/inno/ISCC.EXE)"
-	exit 0;
-fi;
+	# Test if ISCC exist and if wine is present (required for building the installers)
+	if [ ! -f /usr/bin/wine ]; then
+		echo "Skipping packaging of Windows Installer (requires Wine)"
+		exit 0;
+	fi;
+	if [ ! -f /opt/inno/ISCC.exe ]; then
+		echo "Skipping packaging of Windows Installer (requires Inno Setup Compiler code at /opt/inno/ISCC.EXE)"
+		exit 0;
+	fi;
 
-# Download the VC++ and NETFX Redist and expand the FireBird reference libraries
-unzip -o ./Solution\ Items/FirebirdSQL-3.0.3-Embedded.zip -d ./bin/Release/
-wget -q -O - https://aka.ms/vs/17/release/vc_redist.x64.exe > ./installer/vc_redist.x64.exe
-wget -q -O - https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/1f81f3962f75eff5d83a60abd3a3ec7b/ndp48-web.exe > ./installer/netfx.exe
-/usr/bin/wine /opt/inno/ISCC.exe /o./bin/dist ./installer/santedb-icdr.iss /d"MyAppVersion=$1" /d"UNSIGNED=true" /d"MONO_BUILD"
+	# Download the VC++ and NETFX Redist and expand the FireBird reference libraries
+	unzip -o ./Solution\ Items/FirebirdSQL-3.0.3-Embedded.zip -d ./bin/Release/
+	wget -q -O - https://aka.ms/vs/17/release/vc_redist.x64.exe > ./installer/vc_redist.x64.exe
+	wget -q -O - https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/1f81f3962f75eff5d83a60abd3a3ec7b/ndp48-web.exe > ./installer/netfx.exe
+	/usr/bin/wine /opt/inno/ISCC.exe /o./bin/dist ./installer/santedb-icdr.iss /d"MyAppVersion=$1" /d"UNSIGNED=true" /d"MONO_BUILD"
+fi
--- pack-publish-nuget.sh
+++ pack-publish-nuget.sh
@@ -3,25 +3,25 @@ declare build_dir=`pwd`
 
 build_nuget_cwd() {
 	if [ -f *.nuspec ]; then
-        	mono ${build_dir}/.nuget/nuget.exe pack -OutputDirectory bin/publish -prop Configuration=Release -prop VersionNumber=$1
+        	mono ${build_dir}/.nuget/nuget.exe pack -OutputDirectory bin/publish -prop Configuration=$3 -prop VersionNumber=$1
         elif [ -f *.csproj ]; then
-        	dotnet pack --no-build --configuration Release --output bin/publish /p:VersionNumber=$1
+        	dotnet pack --no-build --configuration $3 --output bin/publish /p:VersionNumber=$1
         fi
         if [ -d ./bin/publish ]; then
         	for N in ./bin/publish/*.nupkg; do
-                	dotnet nuget push -s http://oss-baget.fyfesoftware.ca:8080/v3/index.json -k $2 ${N}
+                	dotnet nuget push -s http://$4/v3/index.json -k $2 ${N}
                 done
 
 		for N in ./bin/publish/*.snupkg; do
-			dotnet nuget push -s http://oss-baget.fyfesoftware.ca:8080/v3/index.json -k $2 ${N}
+			dotnet nuget push -s http://$4/v3/index.json -k $2 ${N}
 		done
                 rm -rfv ./bin/publish
         fi
 }
 
-if (( $# < 2 ))
+if (( $# < 4 ))
 then
-	echo "Use: pack-publish-nuget.sh VERSION_ID YOUR_API_KEY"
+	echo "Use: pack-publish-nuget.sh VERSION_ID YOUR_API_KEY CONFIGURATION SERVER"
 	exit -1;
 fi;
 
@@ -29,11 +29,11 @@ for S in *; do
 	if [ -d "${S}" ]; then
 		echo "Entering ${S}"
 		cd "${S}"
-		build_nuget_cwd $1 $2
+		build_nuget_cwd $1 $2 $3 $4
 		for D in *; do
 			if [ -d "${D}" ]; then
 				cd "${D}"
-				build_nuget_cwd $1 $2
+				build_nuget_cwd $1 $2 $3 $4
 				cd ..
 			fi
 		done
