f7445b1c95baae4a6b630e7c3cdbfe79a81411af
Jenkins Server <jenkins@fyfesoftware.ca>
2023-04-06 14:37:43 +0000

    Passed the parameters for configuration to first build step in build

--- build-on-linux.sh
+++ build-on-linux.sh
@@ -21,7 +21,7 @@ fi;
 git checkout $2
 git pull
 ./submodule-pull.sh $2
-msbuild /t:clean /t:restore santedb-server-ext.sln /p:VersionNumber=$1 || exit 911
+msbuild /p:Configuration=$3 /t:clean /t:restore santedb-server-ext.sln /p:VersionNumber=$1 || exit 911
 msbuild /t:build /p:Configuration=$3 santedb-server-ext.sln /p:VersionNumber=$1 /p:NoFirebird=1 || exit 911
 
 
